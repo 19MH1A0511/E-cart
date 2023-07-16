09923c1cd1c4c75508751ff8c1a3f43d7e304467
19MH1A0511 <102719589+19MH1A0511@users.noreply.github.com>
2023-07-17 00:44:57 +0530

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <from>
+                <h1>Login</h1>
+                <h6>enter u details</h6>
+                <div>
+                    <h1>Username</h1>
+                    <input type="text" placeholder='enter username' />
+                    <h1>Password</h1>
+                    <input type="Password" name=''  />
+                </div>
+            </from>
+        </div>
+    )
+} 
